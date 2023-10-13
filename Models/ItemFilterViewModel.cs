29ba6e26d2cb17a15464edb05313371fe6dcca22
uniti <uniti@LAPTOP-VCACRQ2L>
2023-10-13 14:18:10 +0200

    Precommit voor database update

--- Models/ItemFilterViewModel.cs
+++ Models/ItemFilterViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace Verzamelwoede_Dezegaatechtnietstuk.Models
+﻿using Verzamelwoede_NonBroken.Models;
+
+namespace Verzamelwoede_Dezegaatechtnietstuk.Models
 {
     public class ItemFilterViewModel
     {
         public int Id { get; set; }
+        public virtual Filter Filter { get; set; }
         public int FilterId { get; set; }
+        public virtual Item Item { get; set; }
         public int ItemId { get; set; }
 
         public ItemFilterViewModel() { }
