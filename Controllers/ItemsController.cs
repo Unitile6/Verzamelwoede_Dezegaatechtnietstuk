fe980ec8a16f5bcd89645c8318f8505186850c40
uniti <uniti@LAPTOP-VCACRQ2L>
2023-10-19 13:03:38 +0200

    Favourites and filtering added via asp-route-/varname/='value'.

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -14,19 +13,30 @@ namespace Verzamelwoede_Dezegaatechtnietstuk.Controllers
     public class ItemsController : Controller
     {
         private readonly ApplicationDbContext _context;
-        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ItemsController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+        public ItemsController(ApplicationDbContext context)
         {
             _context = context;
-            _webHostEnvironment = webHostEnvironment; 
         }
 
         // GET: Items
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index([FromQuery] bool? favourite = null)
         {
+            var items = _context.Item
+                .Where(x => x.Favourite == true || favourite == null)
+                .ToList();
+
+
             var applicationDbContext = _context.Item.Include(i => i.Category);
-            return View(await applicationDbContext.ToListAsync());
+            if(items != null)
+            {
+                return View(items);
+            }
+            else
+            {
+                return View(await applicationDbContext.ToListAsync());
+            }
+            
         }
 
         // GET: Items/Details/5
@@ -40,12 +50,6 @@ namespace Verzamelwoede_Dezegaatechtnietstuk.Controllers
             var item = await _context.Item
                 .Include(i => i.Category)
                 .FirstOrDefaultAsync(m => m.Id == id);
-
-            //var itemB = await _context.Item
-            //    .Include(i => i.Filters)
-            //    .ThenInclude(u => u.Filters)
-            //    .Where(Item.id ==  id);
-
             if (item == null)
             {
                 return NotFound();
@@ -58,8 +62,6 @@ namespace Verzamelwoede_Dezegaatechtnietstuk.Controllers
         public IActionResult Create()
         {
             ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name");
-         //   var test = _context.Filter.ToList();
-            ViewData["Filters"] = _context.Filter.ToList(); // Haalt alle filters uit database mee naar .
             return View();
         }
 
@@ -68,41 +70,15 @@ namespace Verzamelwoede_Dezegaatechtnietstuk.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Description,CategoryId,Filters,Imageurl,Price,UsesPerYear,Value")] Item item)
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,CategoryId,Imageurl,Price,UsesPerYear,Value,Favourite")] Item item)
         {
-            // Picture mag niet null zijn; betreft de IFormFile picture. Dus deze moet overbrugd worden.
-            //IFormFile picture
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
-                // De code van de pictures in ontvangen van een klasgenoot. Ik heb er aan moeten tweaken om het geheel werkend te krijgen, gezien de Create view 
-                // Telkens stuk ging en ook filter en categorie-koppelingen verdwenen na de initiële submit. Zowel foto als object worden niet aangemaakt.
-                //if (picture != null && picture.Length > 0)
-                //{
-                //    // Genereer een unieke bestandsnaam om conflicten te voorkomen
-                //    string uniqueFileName = Guid.NewGuid().ToString() + "_" + picture.FileName;
-                //    // Bepaal het pad waar het bestand moet worden opgeslagen binnen de wwwroot-map
-                //    string imagesFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
-                //    // Controleer of de map bestaat, zo niet, maak deze aan
-                //    if (!Directory.Exists(imagesFolder))
-                //    {
-                //        Directory.CreateDirectory(imagesFolder);
-                //    }
-
-                //    // Bepaal het volledige pad van het bestand
-                //    string filePath = Path.Combine(imagesFolder, uniqueFileName);
-                //    // Kopieer het bestand naar de opgegeven locatie
-                //    using (var stream = new FileStream(filePath, FileMode.Create))
-                //    {
-                //        await picture.CopyToAsync(stream);
-                //    }
-                //    // Wijs de bestandsnaam toe aan het 'Picture'-veld van het item
-                //    item.Imageurl = uniqueFileName;
-                //}
                 _context.Add(item);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CategoryId"] = new SelectList(_context.Category, "Name", "Id", item.CategoryId);
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name", item.CategoryId);
             return View(item);
         }
 
@@ -119,7 +95,7 @@ namespace Verzamelwoede_Dezegaatechtnietstuk.Controllers
             {
                 return NotFound();
             }
-            ViewData["CategoryId"] = new SelectList(_context.Category, "Name", "Id", item.CategoryId);
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name", item.CategoryId);
             return View(item);
         }
 
@@ -128,7 +104,7 @@ namespace Verzamelwoede_Dezegaatechtnietstuk.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,CategoryId,Imageurl,Price,UsesPerYear,Value")] Item item)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,CategoryId,Imageurl,Price,UsesPerYear,Value,Favourite")] Item item)
         {
             if (id != item.Id)
             {
@@ -155,7 +131,7 @@ namespace Verzamelwoede_Dezegaatechtnietstuk.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Id", item.CategoryId);
+            ViewData["CategoryId"] = new SelectList(_context.Category, "Id", "Name", item.CategoryId);
             return View(item);
         }
 
@@ -200,49 +176,6 @@ namespace Verzamelwoede_Dezegaatechtnietstuk.Controllers
         private bool ItemExists(int id)
         {
           return (_context.Item?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
-
-
-        [HttpPost]
-        [Route("/Images/Upload")] // Added in.
-        public IActionResult Upload(IFormFile imageFile)
-        {
-            if (imageFile != null && imageFile.Length > 0)
-            {
-                // Determine the path where you want to save the image.
-                string imagePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", imageFile.FileName);
-
-                // Save the image file.
-                using (var stream = new FileStream(imagePath, FileMode.Create))
-                {
-                    imageFile.CopyTo(stream);
-                }
-
-                // You should handle additional logic here, like storing the file path in a database.
-            }
-
-            // Redirect or return a response to the client.
-            return RedirectToAction("SomeAction");
-        }
-
-
-        public IActionResult ItemsWithValueLessThan(float? value)
-        {
-            if (!value.HasValue)
-            {
-                return BadRequest("You must pass a value!");
-            }
-            IEnumerable<Item> model = _context.Item
-                .Include(p => p.Category)
-                .Include(p => p.Name)
-                .Where(p => p.Value > value);
-
-            if (!model.Any())
-            {
-                return NotFound($"No items found with a value under {value:C}.");
-            }
-            ViewData["Value"] = value.Value.ToString("C");
-            return View(model);
         }
     }
 }
